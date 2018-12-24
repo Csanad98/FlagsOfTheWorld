87b9b4a93d16a4754edaad3ae6f7a5a0ec75b7a1
Csanád <bakoscsanad@gmail.com>
2018-12-24 21:20:48 +0100

    initial commit after layout parameters

---
+++ app/src/main/java/com/example/macintosh/flagsoftheworld/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.macintosh.flagsoftheworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //add new button to the screen
+        Button butt = new Button(this); //this refers to this activity - button needs to be placed on this activity
+        LinearLayout layout = (LinearLayout) findViewById(R.id.main_activity); // get the layout by id
+        butt.setText("My Button");
+
+        //create layout parameters
+        //LayoutParams is nested inside ViewGroup, that's why we need to reference it this way
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,        //width
+                ViewGroup.LayoutParams.WRAP_CONTENT       //height
+
+        );
+
+        //create layout parameters specific to LinearLayout
+        LinearLayout.LayoutParams params2 = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,        //width
+                ViewGroup.LayoutParams.WRAP_CONTENT       //height
+
+        );
+        params2.weight = 0.33f;
+
+        //
+
+        //apply the parameters to the button
+        butt.setLayoutParams(params);
+
+        layout.addView(butt,1); // add the button, as the second child of the layout
+
+    }
+}
